260d80ddc60d97c298586277874a1c8cd28891f6
Pratham Yadav <pyadav5000@gmail.com>
2021-08-05 06:36:52 +0530

    rewards page completed

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,22 @@
 function myShow() {
     menuBtn.classList.toggle("showMenu");
 }
+
+
+
+// Rewards page js
+const accordionButton = document.getElementsByClassName('accordionTitle');
+
+for (var i = 0; i < accordionButton.length; i++) {
+
+
+    accordionButton[i].addEventListener("click", function () {
+        var accordionContent = this.nextElementSibling;
+        this.parentElement.classList.toggle('visible')
+        if (accordionContent.style.maxHeight) {
+            accordionContent.style.maxHeight = null
+        } else {
+            accordionContent.style.maxHeight = prizeList.scrollHeight + 'px';
+        }
+    })
+}
